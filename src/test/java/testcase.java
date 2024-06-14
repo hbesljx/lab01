11926e5a7afa1a0d021c48cff71715e366fa7412
hbesljx <1273916153@qq.com>
2024-06-14 18:26:59 +0800

    软件工程实验三

--- src/test/java/testcase.java
+++ src/test/java/testcase.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import pojo.Node;
 import tools.Graph;
 import tools.QueryWords;
+import tools.RandomWalk;
 
 import java.io.IOException;
 import java.util.List;
@@ -37,5 +38,15 @@ public class testcase {
         String word1="people";
         String word2="stand";
         System.out.println(QueryWords.queryBridgeWords(nodes,word1,word2));
+    }
+    @Test
+    public void test_5() throws IOException {
+        String filePath="E:\\课程学习\\9_软件工程\\lab01\\src\\main\\resources\\a.txt";
+        List<Node> nodes = Graph.showDirectedGraph(filePath);//生成有向图
+        System.out.println(RandomWalk.randomWalk(nodes));
+    }
+    @Test
+    public void test_6(){
+        System.out.println("to->fight->for->you->in->their->life->they->will->make->room->for->a->spot->never->ever->insist->yourself->to");
     }
 }
